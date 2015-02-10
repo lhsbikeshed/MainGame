b38884d8df23ca4ac97d9c157f0902982dccb2f6
top <tom@imakethin.gs>
2015-02-10 21:32:21 +0000

    start of new drop scene

---
+++ Assets/scenes/NewDropScene/scripts/NewDropScene.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System;
+using UnityOSC;
+
+[System.Serializable]
+public class NewDropScene : GenericScene {
+	
+	
+	GameObject theShip;
+	public Transform skyboxCamera;
+	public Transform planet;
+	
+	public float moveSpeed = -1f;
+	public float altitude = 0f;
+	
+	public override void Start() {
+		theShip = GameObject.Find("TheShip");
+		
+		
+	}
+	
+	public void FixedUpdate() {
+		skyboxCamera.position -= new Vector3(0,0, moveSpeed);
+		altitude = (skyboxCamera.position - planet.position).magnitude;
+	}
+	
+	
+	public override void configureClientScreens(){
+		
+		OSCHandler.Instance.ChangeClientScreen("PilotStation", "radar");			//give the pilot a dockign comp
+		OSCHandler.Instance.ChangeClientScreen("TacticalStation", "weapons");		//give the tactical a weapons screen
+		OSCHandler.Instance.ChangeClientScreen("EngineerStation", "plottingDisplay");			//give the engineer power man console
+		
+	}
+	public override void ProcessOSCMessage(OSCPacket message){
+		String[] msgAddress = message.Address.Split(separator);
+		// [1] = "scene", 2 = "scene name", 3 = thing
+		String target = msgAddress[2];
+		String operation = msgAddress.Length > 2 ? msgAddress[3] : "";
+		
+		
+
+	}
+	
+	
+	
+}
