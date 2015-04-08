b9be01e2f59b9200a841d02edc488e32b6302104
top <tom@imakethin.gs>
2015-04-08 21:24:35 +0100

    beginning of landing computer

---
+++ Assets/scenes/RefuelScene/scripts/LandingTrigger.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LandingTrigger : MonoBehaviour {
+
+	public bool inTrigger = false;
+
+	Vector3 basePos = Vector3.zero;
+	Vector3 localShipPos = Vector3.zero;
+	Bounds b = new Bounds();
+
+	// Use this for initialization
+	void Start () {
+		b = GetComponent<BoxCollider>().bounds;
+		basePos = transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider c){
+		if(c.attachedRigidbody != null && c.attachedRigidbody.name == "TheShip"){
+			inTrigger = true;
+
+			localShipPos = transform.InverseTransformPoint(c.transform.position);
+
+		}
+
+	}
+
+	void OnTriggerEnter(Collider c){
+		if(c.attachedRigidbody != null && c.attachedRigidbody.name == "TheShip"){
+			inTrigger = true;
+		}
+
+	}
+
+	void OnTriggerExit(Collider c){
+		if(c.attachedRigidbody != null && c.attachedRigidbody.name == "TheShip"){
+			inTrigger = false;
+		}
+		
+
+	}
+
+	void OnDestroy(){
+		if(inTrigger){
+			//revert the top level screen
+		}
+	}
+
+	void OnDrawGizmos(){
+		Gizmos.DrawWireCube(b.center, b.extents);
+		Gizmos.DrawRay(new Ray(b.center, Vector3.forward*10));
+		Gizmos.DrawSphere(basePos + localShipPos, 2);
+	}
+}
